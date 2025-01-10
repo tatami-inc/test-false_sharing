6c51a1799a8231d82c8b67dc440907961bfb7261
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2025-01-10 15:45:18 -0800

    Added more worksharing schemes to better diagnose the perf differences.

--- README.md
+++ README.md
@@ -9,15 +9,18 @@ The question is, does this make a difference?
 
 ## Setup
 
-In the _naive_ approach, each thread directly modifies the values in its block across multiple iterations.
-This is most likely to be subject to false sharing at block boundaries.
+We try a few different approaches to characterize the performance of our system:
 
-In the _separate_ approach, each thread allocates a temporary `std::vector` and modifies the values in that thread-specific buffer.
-The final values are copied to the original array when all calculations are done.
-This mitigates false sharing by allowing heap memory to be allocated in thread-specific arenas.
-
-The _aligned_ approach is similar to the separate approach, except that the thread-specific buffers are aligned to a conservative estimate of the cache line size, i.e., 128 bytes.
-This guarantees that no false sharing can occur.
+- In the _naive_ approach, each thread directly modifies the values in its block across multiple iterations.
+This is most likely to be subject to false sharing at block boundaries, provided that the blocks are not inadvertently aligned to cache lines.
+- In the _spaced_ approach, we create a common buffer that has spacing between blocks equal to the cache line (here, assumed to be 128 bytes).
+This should avoid any false sharing as threads can never write to the same cache line.
+- In the _aligned_ approach, we create a common buffer where each block is aligned to the next cache line boundary.
+This should also avoid any false sharing as well as checking the effect of aligned memory access.
+- In the _new-aligned_ approach, each thread allocates a buffer that is aligned to the cache line size and modifies the values in that thread-specific buffer.
+This is similar to the _aligned_ approach but also checks for potential optimizations when the compiler knows that memory is local to a thread.
+- In the _new-vector_ approach, each thread allocates a temporary `std::vector` and modifies the values in that thread-specific vector.
+This hopes to mitigate false sharing by allowing heap memory to be allocated in thread-specific arenas.
 
 To compile:
 
@@ -35,19 +38,19 @@ We can now test all approaches with different sizes of the blocks (i.e., jobs pe
 
 ## Results
 
-Sepcifications:
+We run the test binary with `./build/sharing -n <BLOCK SIZE> -t 4`, using block sizes that aren't a power of 2.
+This avoids unintended alignment to cache lines in the naive approach, which would eliminate false sharing.
+The milliseconds per operation is shown for each approach in the tables below.
 
-- Apple M2 running Sonoma 14.6.1 with Apple clang 15.0.0
+First, on an Apple M2 running Sonoma 14.6.1 with Apple clang 15.0.0:
 
-We run the test binary with `./build/sharing -n <BLOCK SIZE> -t 4`, using block sizes that aren't a power of 2 to avoid accidental alignment in the naive approach.
-The milliseconds per operation is shown for each approach:
-
-| block size | M2 naive | M2 separate | M2 aligned | Intel naive | Intel separate | Intel aligned |
-|------------|----------|-------------|------------|-------------|----------------|---------------|
-|         11 |        8 |           5 |          5 |             |                |               |
-|         21 |       11 |           5 |          5 |             |                |               |
-|         51 |       17 |           6 |          6 |             |                |               |
-|        101 |       26 |          12 |         12 |             |                |               |
-|        201 |       45 |          24 |         24 |             |                |               |
-|        501 |      137 |          52 |         52 |             |                |               |
-|       1001 |      292 |         105 |        105 |             |                |               |
+| block size | naive | spaced | aligned | new-aligned | new-vector | 
+|------------|-------|--------|---------|-------------|------------|
+|         11 |     8 |      5 |       5 |           4 |          4 |
+|         21 |    11 |      5 |       5 |           5 |          5 |
+|         51 |    17 |      9 |       7 |           7 |          7 |
+|        101 |    26 |     14 |      12 |          12 |         12 |
+|        201 |    42 |     31 |      23 |          24 |         23 |
+|        501 |   136 |     95 |      51 |          51 |         52 |
+|       1001 |   292 |    173 |     105 |         103 |        105 |
+|       2001 |   388 |    327 |     207 |         207 |        209 |
--- sharing.cpp
+++ sharing.cpp
@@ -23,17 +23,17 @@ int main(int argc, char* argv[]) {
     // an overly smart compiler from optimizing out everything.
     const int CONSTANT = niter * nthreads + njobs;
 
-    std::vector<double> shared(njobs * nthreads);
+    std::vector<double> naive(njobs * nthreads);
     ankerl::nanobench::Bench().run("naive", [&](){
         std::vector<std::thread> runners;
         runners.reserve(nthreads);
         for (int t = 0; t < nthreads; ++t) {
             runners.emplace_back([&](int t){
                 int start = t * njobs, end = start + njobs;
-                std::iota(shared.begin() + start, shared.begin() + end, CONSTANT + start);
+                std::iota(naive.begin() + start, naive.begin() + end, CONSTANT + start);
                 for (int i = 0; i < niter; ++i) {
                     for (int x = start; x < end; ++x) {
-                        shared[x] += t; // do a very simple modification so that we're mostly memory-bound.
+                        naive[x] += t; // do a very simple modification so that we're mostly memory-bound.
                     }
                 }
             }, t);
@@ -44,8 +44,92 @@ int main(int argc, char* argv[]) {
         }
     });
 
-    std::vector<double> separate(njobs * nthreads);
-    ankerl::nanobench::Bench().run("separate", [&](){
+    constexpr int CACHE_LINE_SIZE = 128;
+    constexpr int DOUBLES_PER_LINE = CACHE_LINE_SIZE / sizeof(double);
+
+    std::vector<double> spaced(njobs * nthreads);
+    ankerl::nanobench::Bench().run("spaced", [&](){
+        std::vector<std::thread> runners;
+        runners.reserve(nthreads);
+        std::vector<double> common((DOUBLES_PER_LINE + njobs) * nthreads);
+
+        for (int t = 0; t < nthreads; ++t) {
+            runners.emplace_back([&](int t){
+                int start = t * njobs, end = start + njobs;
+                auto ptr = common.data() + start + DOUBLES_PER_LINE * t;
+                std::iota(ptr, ptr + njobs, CONSTANT + start);
+                for (int i = 0; i < niter; ++i) {
+                    for (int x = 0; x < njobs; ++x) {
+                        ptr[x] += t; 
+                    }
+                }
+                std::copy_n(ptr, njobs, spaced.begin() + start);
+            }, t);
+        }
+
+        for (auto& tt : runners) {
+            tt.join();
+        }
+    });
+
+    std::vector<double> aligned(njobs * nthreads);
+    ankerl::nanobench::Bench().run("aligned", [&](){
+        std::vector<std::thread> runners;
+        runners.reserve(nthreads);
+        std::vector<double> common((DOUBLES_PER_LINE + njobs) * nthreads);
+        size_t used = 0;
+
+        for (int t = 0; t < nthreads; ++t) {
+            auto common_ptr_raw = reinterpret_cast<void*>(common.data() + used);
+            size_t available = (common.size() - used) * sizeof(double);
+            std::align(CACHE_LINE_SIZE, sizeof(double), common_ptr_raw, available);
+            auto common_ptr = reinterpret_cast<double*>(common_ptr_raw);
+            used = (common_ptr - common.data()) + njobs;
+
+            runners.emplace_back([&](int t, double* ptr){
+                int start = t * njobs, end = start + njobs;
+                std::iota(ptr, ptr + njobs, CONSTANT + start);
+                for (int i = 0; i < niter; ++i) {
+                    for (int x = 0; x < njobs; ++x) {
+                        ptr[x] += t; 
+                    }
+                }
+                std::copy_n(ptr, njobs, aligned.begin() + start);
+            }, t, common_ptr);
+        }
+
+        for (auto& tt : runners) {
+            tt.join();
+        }
+    });
+
+    std::vector<double> new_aligned(njobs * nthreads);
+    ankerl::nanobench::Bench().run("new-aligned", [&](){
+        std::vector<std::thread> runners;
+        runners.reserve(nthreads);
+
+        for (int t = 0; t < nthreads; ++t) {
+            runners.emplace_back([&](int t){
+                int start = t * njobs, end = start + njobs;
+                auto ptr = new (std::align_val_t(CACHE_LINE_SIZE)) double[njobs];
+                std::iota(ptr, ptr + njobs, CONSTANT + start);
+                for (int i = 0; i < niter; ++i) {
+                    for (int x = 0; x < njobs; ++x) {
+                        ptr[x] += t;
+                    }
+                }
+                std::copy_n(ptr, njobs, new_aligned.begin() + start);
+                delete [] ptr;
+            }, t);
+        }
+
+        for (auto& tt : runners) {
+            tt.join();
+        }
+    });
+
+    std::vector<double> new_vector(njobs * nthreads);
+    ankerl::nanobench::Bench().run("new-vector", [&](){
         std::vector<std::thread> runners;
         runners.reserve(nthreads);
 
@@ -60,7 +144,7 @@ int main(int argc, char* argv[]) {
                         ptr[x] += t; 
                     }
                 }
-                std::copy_n(ptr, njobs, separate.begin() + start);
+                std::copy_n(ptr, njobs, new_vector.begin() + start);
             }, t);
         }
 
@@ -69,33 +153,8 @@ int main(int argc, char* argv[]) {
         }
     });
 
-    std::vector<double> aligned(njobs * nthreads);
-    ankerl::nanobench::Bench().run("aligned", [&](){
-        std::vector<std::thread> runners;
-        runners.reserve(nthreads);
-
-        for (int t = 0; t < nthreads; ++t) {
-            runners.emplace_back([&](int t){
-                int start = t * njobs, end = start + njobs;
-                auto ptr = new (std::align_val_t(128)) double[njobs];
-                std::iota(ptr, ptr + njobs, CONSTANT + start);
-                for (int i = 0; i < niter; ++i) {
-                    for (int x = 0; x < njobs; ++x) {
-                        ptr[x] += t;
-                    }
-                }
-                std::copy_n(ptr, njobs, aligned.begin() + start);
-                delete [] ptr;
-            }, t);
-        }
-
-        for (auto& tt : runners) {
-            tt.join();
-        }
-    });
-
-    for (size_t i = 0; i < shared.size(); ++i) {
-        if (shared[i] != separate[i] || aligned[i] != separate[i]) {
+    for (size_t i = 0; i < naive.size(); ++i) {
+        if (naive[i] != new_vector[i] || aligned[i] != new_vector[i] || spaced[i] != new_vector[i] || aligned[i] != new_vector[i]) {
             throw std::runtime_error("oops");
         }
     }
